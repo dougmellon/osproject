c624bfa17dd824b83713046cc9cb101de758237d
DougMellon <dm@dougmellon.com>
2022-02-20 12:26:34 -0700

    Started part 2

---
+++ src/PartA/PartA.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// WaitGroup is used to wait for the program to finish goroutines.
+var wg sync.WaitGroup
+
+// thread.
+func thread(threadName int) {
+	defer wg.Done()
+	fmt.Println("Creating thread ", threadName)
+}
+
+func main() {
+	// Counter for total threads created.
+	totalThreads := 0
+
+	// Start timer.
+	start := time.Now()
+
+	// Create 100,000 threads and then destroy them.
+	for i := 1; i <= 100000; i++ {
+		wg.Add(1)
+		go thread(i)
+		totalThreads++
+	}
+
+	// End timer
+	elapsed := time.Since(start).Milliseconds() // Determine the elapsed time in milliseconds.
+
+	// Wait for the goroutines to finish.
+	wg.Wait()
+
+	// Print out report on start and elapsed times.
+	fmt.Println("Start time: ", start)
+	fmt.Println("Elapsed time: ", elapsed)
+	fmt.Println("Total threads: ", totalThreads)
+}
--- src/PartB/PartB.go
+++ src/PartB/PartB.go
@@ -6,24 +6,36 @@ import (
 	"time"
 )
 
-// WaitGroup is used to wait for the program to finish goroutines.
+// Initiate the waitgroup.
 var wg sync.WaitGroup
 
-// thread
+// Total thread counter.
+var totalThreads = 0
+var totalChildThreads = 0
+
+// thread.
 func thread(threadName int) {
 	defer wg.Done()
-	fmt.Println("Creating thread ", threadName)
+
+	for i := 1; i <= 1000; i++ {
+		go childThread(threadName, i)
+		totalChildThreads++
+	}
+
+}
+
+// childThread.
+func childThread(threadName int, childThread int) {
+	fmt.Printf("Creating thread %d - %d \n", threadName, childThread)
 }
 
 func main() {
-	// Counter for total threads created.
-	totalThreads := 0
 
 	// Start timer.
 	start := time.Now()
 
-	// Create 100,000 threads and then destroy them.
-	for i := 1; i <= 100000; i++ {
+	// Create 100 parent threads.
+	for i := 1; i <= 100; i++ {
 		wg.Add(1)
 		go thread(i)
 		totalThreads++
@@ -38,5 +50,5 @@ func main() {
 	// Print out report on start and elapsed times.
 	fmt.Println("Start time: ", start)
 	fmt.Println("Elapsed time: ", elapsed)
-	fmt.Println("Total threads: ", totalThreads)
+	fmt.Println("Total threads: ", totalThreads+totalChildThreads)
 }
