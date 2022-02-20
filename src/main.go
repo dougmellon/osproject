87b5fafca436c4996c8cbb16dcbd0aba674ab1a6
DougMellon <dm@dougmellon.com>
2022-02-20 01:07:18 -0700

    Finished part 1.

--- src/main.go
+++ src/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+// WaitGroup is used to wait for the program to finish goroutines.
+var wg sync.WaitGroup
+
+// thread
 func thread(threadName int) {
+	defer wg.Done()
 	fmt.Println("Creating thread ", threadName)
 }
 
 func main() {
-
+	// Counter for total threads created.
 	totalThreads := 0
 
 	// Start timer.
@@ -18,6 +24,7 @@ func main() {
 
 	// Create 100,000 threads and then destroy them.
 	for i := 1; i <= 100000; i++ {
+		wg.Add(1)
 		go thread(i)
 		totalThreads++
 	}
@@ -25,7 +32,11 @@ func main() {
 	// End timer
 	elapsed := time.Since(start).Milliseconds() // Determine the elapsed time in milliseconds.
 
-	// Print out report on start and elapsed times
+	// Wait for the goroutines to finish.
+	wg.Wait()
+
+	// Print out report on start and elapsed times.
 	fmt.Println("Start time: ", start)
 	fmt.Println("Elapsed time: ", elapsed)
+	fmt.Println("Total threads: ", totalThreads)
 }
